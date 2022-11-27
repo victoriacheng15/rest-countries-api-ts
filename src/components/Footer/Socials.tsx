d6b605998d9168f6e8bea4da4616cbe9cb764b32
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-27 11:40:49 -0700

    style: run formatter

--- src/components/Footer/Socials.tsx
+++ src/components/Footer/Socials.tsx
@@ -1,37 +1,37 @@
-import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 export function Github() {
-  return (
-    <a
-      href="https://github.com/victoriacheng15"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <FaGithub />
-    </a>
-  );
+	return (
+		<a
+			href="https://github.com/victoriacheng15"
+			target="_blank"
+			rel="noreferrer"
+		>
+			<FaGithub />
+		</a>
+	);
 }
 
 export function Twitter() {
-  return (
-    <a
-      href="https://twitter.com/viktoriacheng15"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <FaTwitter />
-    </a>
-  );
+	return (
+		<a
+			href="https://twitter.com/viktoriacheng15"
+			target="_blank"
+			rel="noreferrer"
+		>
+			<FaTwitter />
+		</a>
+	);
 }
 
 export function Linkedin() {
-  return (
-    <a
-      href="https://www.linkedin.com/in/victoriacheng15/"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <FaLinkedin />
-    </a>
-  );
+	return (
+		<a
+			href="https://www.linkedin.com/in/victoriacheng15/"
+			target="_blank"
+			rel="noreferrer"
+		>
+			<FaLinkedin />
+		</a>
+	);
 }
