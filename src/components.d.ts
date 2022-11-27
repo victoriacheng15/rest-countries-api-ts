f6475d436ed94d031ad0c4e9a8ed7fcb922f7002
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-11-27 11:41:21 -0700

    Feat/setup country card (#3)
    * feat: add useFetchCountries hook
    
    * feat: add countrycard component
    
    * feat: add class for main
    
    * feat: add classes for body tag
    
    * feat: add types for CountryCardProps
    
    * feat: populate content wit CountryCard
    
    * style: run formatter

---
+++ src/components.d.ts
@@ -0,0 +1,9 @@
+interface CountryCardProps {
+	alphaCode?: number;
+	name: string;
+	flags: string;
+	alt: string;
+	population: number;
+	region: string;
+	countryCapital: string;
+}
---
+++ src/hooks/fetchUrls.ts
@@ -0,0 +1,6 @@
+const url = {
+	countries: "https://restcountries.com/v3.1/all",
+	country: "https://restcountries.com/v3.1/alpha/",
+};
+
+export default url;
