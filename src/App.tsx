ae10599fe6d9b4774d9fa55a99696d1cf659d3a4
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-27 01:56:36 -0700

    feat: set up class for switch modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import { useThemeContext, ThemeContextType } from "./contexts/ThemeContext";
 
 function App() {
+	const { dark }: ThemeContextType = useThemeContext();
 
 	return (
-		<div className={`min-h-screen font-nuito`}>
+		<div className={`min-h-screen font-nuito ${dark && "dark"}`}>
 			<Header />
 		</div>
 	);
