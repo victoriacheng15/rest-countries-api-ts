509493e21e504a16077adeab27722589ec5431b7
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-27 11:40:30 -0700

    feat: populate content wit CountryCard

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,28 @@
+import CountryCard from "../components/CountryCard";
+import useFetchCountries from "../hooks/useFetchCountries";
+
 function Home() {
-	return <div>Home</div>;
+	const { countries } = useFetchCountries();
+	return (
+		<>
+			<section className="mx-auto flex max-w-7xl flex-wrap justify-between gap-8">
+				{countries
+					.slice(0, 70)
+					.map(({ name, capital, flags, region, population, cca2 }: any) => (
+						<CountryCard
+							key={cca2}
+							alphaCode={cca2}
+							name={name.official}
+							countryCapital={capital || "N/A"}
+							region={region}
+							flags={flags.png}
+							alt={name.official}
+							population={population}
+						/>
+					))}
+			</section>
+		</>
+	);
 }
 
 export default Home;
