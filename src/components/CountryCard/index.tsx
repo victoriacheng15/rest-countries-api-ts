6a0d79e16f87ed6fbea75b4a9b566ad404fac013
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-28 12:20:39 -0700

    chore: rename props to cca2 and capital and add css style for titles

--- src/components/CountryCard/index.tsx
+++ src/components/CountryCard/index.tsx
@@ -1,23 +1,30 @@
 function CountryCard({
-	alphaCode,
+	cca2,
 	name,
 	flags,
 	alt,
 	population,
 	region,
-	countryCapital,
+	capital,
 }: CountryCardProps) {
 	return (
 		<>
-			<article className="flex flex-col overflow-hidden rounded-lg bg-lightGray-800 shadow-lg shadow-gray-600 duration-300 ease-in-out hover:scale-110 dark:bg-darkBlue-700 dark:shadow-gray-600 lg:basis-72">
+			<article className="flex max-w-xs flex-1 basis-[265px] flex-col overflow-hidden rounded-lg bg-lightGray-800 shadow-lg shadow-gray-600 duration-300 ease-in-out hover:scale-110 dark:bg-darkBlue-700 dark:shadow-gray-600 lg:basis-72">
 				<img src={flags} alt={alt} className="object-fit h-[180px] w-full" />
 				<div className="flex flex-col gap-4 p-3">
 					<h2 className="text-lg font-extrabold text-darkBlue-900 dark:text-lightGray-800">
 						{name}
 					</h2>
-					<p>Population: {population.toLocaleString()}</p>
-					<p>Region: {region}</p>
-					<p>Capital: {countryCapital}</p>
+					<p>
+						<span className="font-bold capitalize">Population:</span>{" "}
+						{population.toLocaleString()}
+					</p>
+					<p>
+						<span className="font-bold capitalize">Region:</span> {region}
+					</p>
+					<p>
+						<span className="font-bold capitalize">Capital:</span> {capital}
+					</p>
 				</div>
 			</article>
 		</>
