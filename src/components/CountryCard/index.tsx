419a8dc8c7895ab7effb062a255a156fb383204e
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-30 13:49:16 -0700

    feat: populate country detailed information and change code to cca3

--- src/components/CountryCard/index.tsx
+++ src/components/CountryCard/index.tsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom";
+import Paragraph from "../Paragraph";
+
 function CountryCard({
-	cca2,
+	cca3,
 	name,
 	flags,
 	alt,
@@ -9,24 +12,23 @@ function CountryCard({
 }: CountryCardProps) {
 	return (
 		<>
-			<article className="flex max-w-xs flex-1 basis-[265px] flex-col overflow-hidden rounded-lg bg-lightGray-800 shadow-lg shadow-gray-600 duration-300 ease-in-out hover:scale-110 dark:bg-darkBlue-700 dark:shadow-gray-600 lg:basis-72">
-				<img src={flags} alt={alt} className="object-fit h-[180px] w-full" />
-				<div className="flex flex-col gap-4 p-3">
-					<h2 className="text-lg font-extrabold text-darkBlue-900 dark:text-lightGray-800">
-						{name}
-					</h2>
-					<p>
-						<span className="font-bold capitalize">Population:</span>{" "}
-						{population.toLocaleString()}
-					</p>
-					<p>
-						<span className="font-bold capitalize">Region:</span> {region}
-					</p>
-					<p>
-						<span className="font-bold capitalize">Capital:</span> {capital}
-					</p>
-				</div>
-			</article>
+			<Link
+				className="flex max-w-xs flex-1 basis-[265px] duration-300 ease-in-out hover:scale-110"
+				to={`/country/${cca3}`}
+			>
+				<article className="w-full overflow-hidden rounded-lg bg-lightGray-800 shadow-lg shadow-gray-600 dark:bg-darkBlue-700 dark:shadow-gray-600 lg:basis-72">
+					<img src={flags} alt={alt} className="object-fit h-[180px] w-full" />
+					<div className="flex flex-col gap-4 p-3">
+						<h2 className="text-lg font-extrabold ">{name}</h2>
+						<Paragraph
+							title="population"
+							content={population.toLocaleString()}
+						/>
+						<Paragraph title="region" content={region} />
+						<Paragraph title="capital" content={capital} />
+					</div>
+				</article>
+			</Link>
 		</>
 	);
 }
