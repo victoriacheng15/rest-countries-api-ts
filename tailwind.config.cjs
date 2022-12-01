8bbea4ef84d3097c6a48650928d03ed7a58e6d7a
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-01 13:42:57 -0700

    feat: add spin-slow animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
+    animation:{
+      "spin-slow": "spin 3s linear infinite"
+    },
     extend: {
       fontFamily: {
         nuito: ['Nunito Sans', 'sans-seri'],
