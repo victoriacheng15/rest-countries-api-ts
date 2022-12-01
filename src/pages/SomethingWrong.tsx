2e8b94d3f67972340bd0b0e4ab4acd960f6e5a6a
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-01 13:42:17 -0700

    feat: add SomethingWrong page for error

---
+++ src/pages/SomethingWrong.tsx
@@ -0,0 +1,13 @@
+import { BackHomeBtn } from "../components"
+import ErrorPage from "../components/ErrorPage"
+
+function SomethingWrong() {
+  return (
+    <>
+      <BackHomeBtn />
+      <ErrorPage />
+    </>
+  )
+}
+
+export default SomethingWrong
