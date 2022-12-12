5daa98962eb74a3cf700c12ee9473e166851ddf3
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-11 22:30:06 -0700

    feat: add custom render file to render components with providers

---
+++ src/test-utils.tsx
@@ -0,0 +1,30 @@
+import { ReactElement } from "react";
+import { render, RenderOptions } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { CountriesProvider } from "./contexts/CountriesContext";
+import { PaginationProvider } from "./contexts/PaginationContext";
+
+const AllProviders = ({ children }: ChildrenProps) => {
+	return (
+		<BrowserRouter>
+			<ThemeProvider>
+				<CountriesProvider>
+					<PaginationProvider>{children}</PaginationProvider>
+				</CountriesProvider>
+			</ThemeProvider>
+		</BrowserRouter>
+	);
+};
+
+const customRender = (
+	ui: ReactElement,
+	options?: Omit<RenderOptions, "wrapper">,
+) =>
+	render(ui, {
+		wrapper: AllProviders,
+		...options,
+	});
+
+export * from "@testing-library/react";
+export { customRender as render };
