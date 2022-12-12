91accb3448fdf8ef2ce3dee5349d2c6e71c3a2ed
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-12-12 09:09:18 -0700

    Add custom render (#12)
    * chore: move test files to __tests__ folder and reorganized test files
    
    * feat: add custom render file to render components with providers
    
    * style: the code has been formatted by rome

---
+++ src/__tests__/Pagination.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "../test-utils";
+import { Pagination } from "../components";
+
+describe("Pagination", () => {
+	beforeEach(() => {
+		render(<Pagination />);
+	});
+
+	it("should render pagination correctly", () => {
+		const pagination = screen.getByRole("navigation");
+		expect(pagination).toBeInTheDocument();
+	});
+});
