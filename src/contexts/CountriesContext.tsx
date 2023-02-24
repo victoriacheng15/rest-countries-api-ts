034ba9035455b4ad242b81c2aabf32e920adae94
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-24 12:10:09 -0700

    refactor: extract types and reducer out from context files

--- src/contexts/CountriesContext.tsx
+++ src/contexts/CountriesContext.tsx
@@ -6,84 +6,38 @@ import {
 	useMemo,
 } from "react";
 import useFetchCountries from "../hooks/useFetchCountries";
-
-interface FormState {
-	search: string;
-	select: string;
-}
-
-interface CountriesContextType extends CountriesState, FormState {
-	setSearch: (search: string) => void;
-	setSelect: (select: string) => void;
-}
+import { SET_SEARCH, SET_SELECT } from "../utils/constants";
+import { formReducer } from "../utils/reducers";
 
 const CountriesContext = createContext({} as CountriesContextType);
-
-enum ActionTypes {
-	SET_SEARCH = "setSearch",
-	SET_SELECT = "setSelect",
-}
-
-interface SearchAction {
-	type: ActionTypes.SET_SEARCH;
-	payload: string;
-}
-
-interface SelectAction {
-	type: ActionTypes.SET_SELECT;
-	payload: string;
-}
-
-type FormActions = SearchAction | SelectAction;
 
 const initialState = {
 	search: "",
 	select: "",
 };
 
-const formReducer = (state: FormState, action: FormActions) => {
-	switch (action.type) {
-		case ActionTypes.SET_SEARCH:
-			return {
-				...state,
-				search: action.payload,
-			};
-		case ActionTypes.SET_SELECT:
-			return {
-				...state,
-				select: action.payload,
-			};
-		default:
-			return state;
-	}
-};
-
-export function CountriesProvider({ children }: ChildrenProps) {
+export function CountriesProvider({ children }: ChildrenProp) {
 	const { countries, loading, error } = useFetchCountries();
 	const [{ search, select }, dispatch] = useReducer(formReducer, initialState);
 
 	const setSearch = useCallback((search: string) => {
-		dispatch({
-			type: ActionTypes.SET_SEARCH,
-			payload: search,
-		});
+		dispatch({ type: SET_SEARCH, payload: search });
 	}, []);
 
 	const filteredCountries = useMemo(() => {
-		return countries.filter((c) =>
-			c.name.official.toLowerCase().includes(search.toLowerCase()),
+		return countries.filter(({ name: { official } }) =>
+			official.toLowerCase().includes(search.toLowerCase()),
 		);
 	}, [countries, search]);
 
 	const setSelect = useCallback((select: string) => {
-		dispatch({
-			type: ActionTypes.SET_SELECT,
-			payload: select,
-		});
+		dispatch({ type: SET_SELECT, payload: select });
 	}, []);
 
 	const selectRegion = useMemo(() => {
-		return countries.filter(({ region }) => region.includes(select));
+		return countries.filter(({ region }) =>
+			region.includes(select),
+		);
 	}, [countries, select]);
 
 	const formParameter = search ? filteredCountries : selectRegion;
