ba2f977adf223aad10c69ebf20dd278acbe0fc24
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-02-24 10:49:44 -0700

    Restructure src folder (#20)
    * chore: move to component folder and rename to the component name instead of index
    
    * fix: correct typo for BackHomeBtn

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,5 @@ export { default as Form } from "./Form";
 export { default as CountryCard } from "./CountryCard";
 export { default as Pagination } from "./Pagination";
 
-export { default as BackHomeBtn } from "./BackHomBtn";
+export { default as BackHomeBtn } from "./BackHomeBtn";
 export { default as DetailedCountry } from "./DetailedCountry";
